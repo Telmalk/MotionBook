b28391708623458093493517b541b566ee26bae7
Clément Haller <clement.haller@hetic.net>
2018-02-15 20:30:18 +0100

    push presque final

--- change.php
+++ change.php
@@ -1,7 +1,9 @@
 <?php
+session_start();
+require_once './connexion.php';
 
-require 'connexion.php';
-$requete = "SELECT 
+$requete = "SELECT
+  `user_id`, 
   `username`, 
   `email`,
   `password` 
@@ -11,19 +13,91 @@ WHERE
   `user_id` = :user_id
 ;";
 $stmt = $conn->prepare($requete);
-$stmt->bindValue(':user_id', $_GET['user_id']);
+$stmt->bindValue(':user_id', $_SESSION['user']['id']);
 $stmt->execute();
 $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Index &bull; Fruity Motion</title>
+
+    <meta charset="utf-8">
+    <meta value="notranslate" name="google">
+    <meta content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0" name="viewport">
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/grid.css">
+    <!--[if gte IE 9]
+        <style type="text/css">
+            .gradient {
+                filter: none;
+            }
+        </style>
+    <![endif]-->
+</head>
+<body>
+<?php
+    include "header.php";
 ?>
 
+<form action="update.php" method="post" class="app_content" enctype="multipart/form-data">
+    <ul class="grid-container settings">
+        <li>
+            <h2>Profil</h2>
+        </li>
 
+        <input type="hidden" name="user_id" value="<?=$row['user_id']?>">
 
+        <li>
+            <label for="username">User name</label>
+            <div><input type="text" name="username" value="<?=$row['username']?>"></div>
+        </li>
+        <li>
+            <label for="about">About you</label>
+            <div><textarea name="about" placeholder="About you"></textarea></div>
+        </li>
+        <li>
+            <label for="location">Avatar</label>
+            <div><input type="file" name="avatar"></div>
+        </li>
+        <li>
+            <label for="website">Website</label>
+            <div><input type="text" name="website" placeholder="http://"></div>
+        </li>
+    </ul>
 
-<form  action="update.php" method="post">
-    <input type="hidden" name="user_id" value="<?=$_GET['user_id']?>"> <br>
-    <label>username</label><input type="text" name="username" value="<?=$row['username']?>"> <br>
-    <label for="">email</label><input type="email" name="email" value="<?=$row['email']?>"> <br>
-    <label>password</label><input type="password" name="password"><br>
-    <input type="submit">
+    <ul class="grid-container settings">
+        <li>
+            <h2>Basic Info</h2>
+        </li>
+        <li>
+            <label for="email">Email adress</label>
+            <div><input type="email" name="email" value="<?=$row['email']?>"></div>
+        </li>
+        <li>
+            <label for="email">Password</label>
+            <div><input type="password" name="password"></div>
+        </li>
+    </ul>
+
+    <div class="grid-container settings">
+        <button class="btn-default">Delete account</button>
+        <div class="right">
+            <button class="btn-default">Cancel</button>
+            <button type="submit" class="btn-submit">Save settings</button>
+        </div>
+
+    </div>
 </form>
 
+
+<footer>
+    MotionBook &copy; 2014 &bull; Designed & developed with love by Kevin Manssat
+</footer>
+
+<script src="js/jquery.js"></script>
+<script src="js/salvattore.min.js"></script>
+</body>
+</html>
--- update.php
+++ update.php
@@ -1,18 +1,35 @@
 <?php
+/**
+ * Created by PhpStorm.
+ * User: travailleur
+ * Date: 15/02/2018
+ * Time: 18:52
+ */
+session_start();
+
 require 'connexion.php';
 $requete = "UPDATE
 `user`
 SET
 `username` = :username,
 `email` = :email,
-`password` = :password
+`password` = :password,
+`avatar` = :avatar
 WHERE
 user_id = :user_id
 ;";
 $stmt = $conn->prepare($requete);
-$stmt->bindParam(':username', $_POST['username']);
+$stmt->bindValue(':username', $_POST['username']);
 $stmt->bindValue(':email', $_POST['email']);
 $stmt->bindValue(':password', hash("sha256", $_POST['password']));
+var_dump($_FILES);
+if ($_FILES['avatar']['name'] != "") {
+    echo "je sios la poto";
+    move_uploaded_file($_FILES['avatar']['tmp_name'], "asset/img/avatar/" . $_POST['username'] . "_" . $_SESSION['user']["id"]. ".jpg");
+    $stmt->bindValue(':avatar', "asset/img/avatar/" . $_POST['username'] . "_" . $_SESSION['user']["id"]. ".jpg");
+} else {
+    $stmt->bindValue(':avatar', "./asset/img/avatar/avatar_default.png");
+}
 if ($_POST["password"] != "") {
     $stmt->bindValue(':user_id', $_POST['user_id']);
 } else {
@@ -29,7 +46,6 @@ if ($_POST["password"] != "") {
     $passwordStmt->execute();
     $row = $passwordStmt->fetch(PDO::FETCH_ASSOC);
     $stmt->bindValue(':password', $row["password"]);
-
 }
 $stmt->execute();
 header('Location: index.php');
